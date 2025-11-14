66101cb2434f8b9938702670426680934ccc3517
Krista T <krista.tomicic@gmail.com>
2025-11-14 09:59:52 +0100

    feat: Added utility function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+
+
+
 def main():
     print("Hello from semantic-versioning-test!")
 
+def add_numbers(a, b):
+    print(f"Adding numbers...{a} + {b}")
+    return a + b
+
 
 if __name__ == "__main__":
+    add_numbers(3, 5)
     main()
